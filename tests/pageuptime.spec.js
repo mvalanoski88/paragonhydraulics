7d7dce338d2f4dab50e28fadb3e93c82282700fc
Matt Valanoski <mvalanoski@gmail.com>
2026-04-26 15:44:37 -0400

    updated file issues

--- tests/pageuptime.spec.js
+++ tests/pageuptime.spec.js
@@ -9,5 +9,5 @@ test('page uptime', async ({ page }) => {
 
   // expect heading with same name as link and capture screenshot
   await expect(page.getByRole('heading', { name: 'Contact Us' })).toBeVisible();
-  await expect(page).toHaveScreenshot('contactuspage-chromium-win32-chromium-linux.png');
+  await expect(page).toHaveScreenshot('contactuspage.png');
 });
