8c1623c55d95a5816e640eb7757c031b69a0bd0a
Matt Valanoski <mvalanoski@gmail.com>
2026-04-26 15:22:41 -0400

    testing playwright script

---
+++ tests/pageuptime.spec.js
@@ -0,0 +1,13 @@
+import { test, expect } from '@playwright/test';
+
+test('page uptime', async ({ page }) => {
+  // navigate to homepage
+  await page.goto('https://dev.paragonhydraulics.com');
+
+  // click the fist a element with the name contact us
+  await page.getByRole('link', { name: 'Contact Us' }).first().click();
+
+  // expect heading with same name as link and capture screenshot
+  await expect(page.getByRole('heading', { name: 'Contact Us' })).toBeVisible();
+  await expect(page).toHaveScreenshot('contactuspage.png');
+});
